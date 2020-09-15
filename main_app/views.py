80bde982811465b31d67b47e44a3e824d307a926
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-09-15 17:39:14 -0600

    finished deliverable

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .models import Dog
 # Create your views here.
 from django.http import HttpResponse
 
@@ -8,3 +9,11 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def dogs_index(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', {'dogs': dogs})
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dog': dog })
