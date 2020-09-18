b0f311554048df7027af9a84da8a63c26c3764e8
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-09-17 18:13:35 -0600

    all done

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Dog, Feeding, Toy
 
 admin.site.register(Dog)
 admin.site.register(Feeding)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from django.views.generic import ListView, DetailView
+from .models import Dog, Toy
 from .forms import FeedingForm
 
 
@@ -19,10 +20,12 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
+    toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request, 'dogs/detail.html', {
         'dog': dog,
-        'feeding_form': feeding_form
+        'feeding_form': feeding_form,
+        'toys': toys_dog_doesnt_have
     })
 
 
@@ -47,4 +50,26 @@ def add_feeding(request, dog_id):
         new_feeding = form.save(commit=False)
         new_feeding.dog_id = dog_id
         new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def assoc_toy(request, dog_id, toy_id):
+    Dog.objects.get(id=dog_id).toys.add(toy_id)
     return redirect('detail', dog_id=dog_id)
